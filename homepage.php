f68e886cb31c4fc0e961ff00a5415ed3b4a1387c
tejaspundlik <101443790+tejaspundlik@users.noreply.github.com>
2023-02-11 20:45:14 +0530

    Change Icon fix navbar issue

--- homepage.php
+++ homepage.php
@@ -5,6 +5,7 @@
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="icon" type="image/x-icon" href="./Assets/favicon.png">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="./styles.css" />
@@ -15,7 +16,7 @@
 	<nav class="navbar navbar-expand-lg fixed-top">
 		<div class="container-fluid">
 			<a class="navbar-brand" href="./homepage.php">
-				<p class="navicon">Cyber Safety</p>
+				<img width="15%" src="./Assets/homeicon.png" />
 			</a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
