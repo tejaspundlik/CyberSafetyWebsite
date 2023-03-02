0e15346bd476aaf12e9df30fea60c159b2cafe0b
tejaspundlik <101443790+tejaspundlik@users.noreply.github.com>
2023-03-02 14:46:14 +0530

    Complete Mail function

--- contactpage.php
+++ contactpage.php
@@ -1,6 +1,32 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+if (isset($_POST['email']) && $_POST['email'] != '') {
+	$mail = new PHPMailer(true);
+	$mail->isSMTP();
+	$mail->Host       = 'smtp.gmail.com';
+	$mail->SMTPAuth   = true;
+	$mail->Username   = 'kjprojects23@gmail.com';
+	$mail->Password   = 'zstnsitmqzmdmsky';
+	$mail->SMTPSecure = 'ssl';
+	$mail->Port       = 465;
+
+	$mail->setFrom($_POST['email']);
+	$mail->addAddress('kjprojects23@gmail.com');
+	$mail->isHTML(true);
+	$mail->Subject = 'Feedback from ' . $_POST['Name'];
+	$mail->Body    = $_POST['message'];
+	$mail->send();
+}
 ?>
+
 <html lang="en">
 
 <head>
@@ -71,7 +97,7 @@
 				<img src="./Assets/mail.gif" />
 			</div>
 			<div class="contact-form">
-				<form action="send.php" method="POST">
+				<form action="./contactpage.php" method="POST">
 					<h3 class="title">Let's get in touch</h3>
 					<div class="input-container">
 						<input type="text" name="Name" class="input" id="Name" autocomplete="off" />
@@ -88,7 +114,7 @@
 						<label name="feedback" for="message">Feedback</label>
 						<span>Message</span>
 					</div>
-					<input type="submit" name="submit" value="Submit" class="btn1" />
+					<input type="submit" value="Submit" class="btn1" />
 				</form>
 			</div>
 		</div>
