d157203911d73b0b67196009c465b50c4447b23a
tejaspundlik <101443790+tejaspundlik@users.noreply.github.com>
2023-03-26 12:02:01 +0530

    Have to do the donation page using forms

--- quiz.php
+++ quiz.php
@@ -5,10 +5,10 @@
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>Support Us</title>
+	<title>Review Page</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+	<link rel="stylesheet" href="./StyleSheet/quiz.css" />
 	<link rel="stylesheet" href="./StyleSheet/navbar.css" />
-	<link rel="stylesheet" href="./StyleSheet/donationPage.css" />
 </head>
 
 <body>
@@ -58,28 +58,33 @@
 			</div>
 		</div>
 	</nav>
-	<div class="main">
-		<div class="message">
-			<img class="image" src="./Assets/donation.jpg" />
-			<p>
-				We are proudly non-profit, non-corporate and non-compromised.
-				Thousands of people like you help us stand up for a safe internet for
-				all. We rely on donations to carry out our mission to keep the web
-				secure for all.
-			</p>
+	<div class="quiz-container" id="quiz">
+		<div class="quiz-header">
+			<h2 id="question">Question Text</h2>
+			<ul>
+				<li>
+					<input type="radio" name="answer" id="a" class="answer" />
+					<label for="a" id="a_text">Answer</label>
+				</li>
+				<li>
+					<input type="radio" name="answer" id="b" class="answer" />
+					<label for="b" id="b_text">Answer</label>
+				</li>
+				<li>
+					<input type="radio" name="answer" id="c" class="answer" />
+					<label for="c" id="c_text">Answer</label>
+				</li>
+				<li>
+					<input type="radio" name="answer" id="d" class="answer" />
+					<label for="d" id="d_text">Answer</label>
+				</li>
+			</ul>
 		</div>
-		<div class="donation">
-			<h1>Donate Now</h1>
-			<p>Donate what you think is a fair amount</p>
-			<input placeholder="Amount In Rupees" id="amount" /><br />
-			<div style="max-width: 10rem; margin: 0 auto 0 auto">
-				<button class="button" id="donationButton">
-					<p>Donate</p>
-				</button>
-			</div>
-		</div>
+		<button class="button" id="submit">
+			<p>Next</p>
+		</button>
 	</div>
-	<script src="./DonationBackend/Donation.js"></script>
+	<script src="./QuizBackend/quiz.js"></script>
 </body>
 
 </html>
