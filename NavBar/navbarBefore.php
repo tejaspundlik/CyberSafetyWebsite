901bf0be9cdac3793a2ccc22a044a46fdc2991f3
tejaspundlik <101443790+tejaspundlik@users.noreply.github.com>
2023-04-05 15:40:06 +0530

    Fix navbar

--- NavBar/navbarBefore.php
+++ NavBar/navbarBefore.php
@@ -2,68 +2,56 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
-    <link rel="stylesheet" href="../NavBar/navbar.css" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <title></title>
+    <link rel="stylesheet" href="../NavBar/newNavbar.css" />
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <title>Document</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-xxl fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="../homepage.php">
-                <img width="15%" style="margin-left: 1rem" src="../Assets/homeicon.png" />
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent" style="max-width: 80%">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <div class="navicon">
-                                <img src="../Assets/virus.gif" width="25.5%" />
-                                <p>Virus Checker</p>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../passchecker.php">
-                            <div class="navicon">
-                                <img src="../Assets/lock.gif" width="24.5%" />
-                                <p>Password Tester</p>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../reviewpage.php">
-                            <div class="navicon">
-                                <img src="../Assets/recommend.gif" width="28.5%" />
-                                <p>Products</p>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../contactpage.php">
-                            <div class="navicon">
-                                <img src="../Assets/location.gif" width="27.5%" />
-                                <p>Contact Us</p>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../signInPage.php">
-                            <div class="navicon">
-                                <img src="../Assets/signin.png" width="80%" />
-                                <p>Sign-In</p>
-                            </div>
-                        </a>
-                    </li>
-                </ul>
-            </div>
+    <nav class="navbar navbar-expand-xxl navbar-light">
+        <a style="margin:0;padding:0;" href="../homepage.php"><img width="5%" style="margin-left: 1%" src="../Assets/homeicon.png" /></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="../newsletterPage.php">
+                        <img class="navImage" src="../Assets/virus.gif" />
+                        <p class="navText">Newsletter</p>
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="../passchecker.php">
+                        <img class="navImage" src="../Assets/virus.gif" />
+                        <p class="navText">Password Test</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../contactpage.php">
+                        <img class="navImage" src="../Assets/virus.gif" />
+                        <p class="navText">Contact Us</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../donation.php">
+                        <img class="navImage" src="../Assets/virus.gif" />
+                        <p class="navText">Donate</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../signInPage.php">
+                        <img class="navImage" src="../Assets/virus.gif" />
+                        <p class="navText">Sign In</p>
+                    </a>
+                </li>
+            </ul>
         </div>
     </nav>
 </body>
