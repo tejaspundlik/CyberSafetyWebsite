aee8428dcba5a2711e67491f3ee9afc1a72768a6
tejaspundlik <101443790+tejaspundlik@users.noreply.github.com>
2023-04-30 01:44:46 +0530

    Fix footer css

--- Footer/footer.php
+++ Footer/footer.php
@@ -6,16 +6,20 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="stylesheet" href="../Footer/footer.css" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="../StyleSheet/font.css" />
 </head>
 
 <body>
 	<div class="footerouter">
-		<div>
-			<h4 class="footerh4">Donate</h4>
-			<p class="footerp">Help support our cause by making a donation today.</p>
-			<a class="footera" href="../donation.php">Donate Now</a>
+		<div class="left">
+			<h4 class="footerh4">Donation</h4>
+			<p class="footerp">Help support our cause by making a donation today, every donation no matter how small helps us.</p>
+			<a class="footera" href="../donation.php">Donate</a>
 		</div>
-		<div>
+		<div class="right">
 			<h4 class="footerh4">Newsletter</h4>
 			<p class="footerp">
 				Sign up for our newsletter to stay up-to-date on our latest news and
